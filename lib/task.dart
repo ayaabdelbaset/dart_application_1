abc20feef7646c7f0a6445ed1fb93282ab2a99be
ayaabdelbaset <ayaabdelbaset1111@gmail.com>
2026-07-26 17:14:56 +0300

    Update task 5

--- lib/task.dart
+++ lib/task.dart
@@ -1,92 +1,54 @@
 void main() {
-  // TASK 1
-  for (int i = 1; i <= 30; i++) {
-    if (i % 3 == 0) {
-      print("Fizz");
-    } else {
-      print(i);
+
+    print(recArea(5.5, 6));
+  isPalindrome('Race car');
+    print (userProfile(name: 'Aya' , city: 'shebin'));
+  List<int> nums = [10, 2, 7, 5, 8, 9, 6, 200];
+  print(filterNumbers(nums, isEven));
+  print(filterNumbers(nums, isGreater10));
+
+}
+
+// TASK 1
+double recArea(double h, double w) {
+  double area = h * w;
+  return area;
+}
+
+// TASK 2
+bool? isPalindrome(String t) {
+  String t2 = t.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
+
+  String reversed = t2.split('').reversed.join(); // OR
+
+  if (t2 == reversed) {
+    print(' True');
+  } else {
+    print('False');
+  }
+}
+
+//TASK 3
+
+String? userProfile({required String name, String? city, int? age}) {
+  return "Name: $name, City: ${city ?? 'Unknown'}, Age: ${age ?? 'Unknown'}";
+}
+
+//TASK 4
+bool isEven(int num) {
+  return num % 2 == 0;
+}
+
+bool isGreater10(int num) {
+  return num > 10;
+}
+
+List<int>? filterNumbers(List<int> nums, bool Function(int) test) {
+  List<int> result = [];
+  for (int num in nums) {
+    if (test(num)) {
+      result.add(num);
     }
   }
-
-  // TASK 2
-
-  List<String> fruits = ['apple', 'banana', 'mango', 'kiwi'];
-  for (int i = 0; i < fruits.length; i++) {
-    print('$i : ${fruits[i]}');
-  }
-
-  // Task 3
-
-  List<int> scores = [55, 82, 90, 43, 77];
-  int sum = 0;
-  double average = 0;
-  for (int i = 0; i < scores.length; i++) {
-    sum += scores[i];
-    average = sum / scores.length;
-  }
-  print(""" Sum : $sum  Average: $average""");
-
-
-
-  // TASK 4
-
-  Map<String, int> ages = {'Sara': 20, 'Omar': 23, 'Laila': 19};
-  ages.forEach((key, value) {
-    print('$key is $value years old');
-  });
-
-
-
-  //TASK 5
-
-  List<int> numbers = [12, 45, 3, 89, 21, 67];
-  int max = numbers[0];
-  for (int i = 0; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-      max = numbers[i];
-    }
-  }
-  print(max);
-
-
-
-  // TASK 6
-
-  List<Map<String, dynamic>> products = [
-    {'name': 'Book', 'price': 40},
-    {'name': 'Pen', 'price': 5},
-    {'name': 'Bag', 'price': 120},
-  ];
-  num total = 0;
-  for (int i = 0; i < products.length; i++) {
-    total += products[i]['price'];
-    print('${products[i]['name']} ${products[i]['price']}  ');
-  }
-  print('Total is $total');
-
-
-
-
-  // Bonus
-  List<Map<String, dynamic>> products = [
-    {'name': 'Book', 'price': 40, 'qty': 2},
-    {'name': 'Pen', 'price': 5, 'qty': 10},
-    {'name': 'Bag', 'price': 120, 'qty': 1},
-  ];
-
-  num totalCost = 0;
-  int total = 0;
-  int max = 0;
-  String? name;
-  for (int i = 0; i < products.length; i++) {
-    totalCost += products[i]['price'] * products[i]['qty'];
-    total = products[i]['price'] * products[i]['qty'];
-    if (total > max) {
-      max = total;
-      name = products[i]['name'];
-    }
-  }
-  print(name);
-  print('totalCost is $totalCost');
-
+  return result;
 }
